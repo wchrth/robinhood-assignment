8cae791733bd5a29db5b374bf21c0d0b236b3c00
Watcharathorn Laikhunakhorn <watcharathorn@icloud.com>
2023-08-09 17:57:21 +0700

    Update entity

--- domain/entity/appointment.go
+++ domain/entity/appointment.go
@@ -6,7 +6,7 @@ type Appointment struct {
 	ID          uint
 	Description string `gorm:"size:200;not null"`
 	Comments    []Comment
-	UserID      uint
+	UserID      uint `gorm:"not null"`
 	User        User
 	IsArchived  bool      `gorm:"default:false;not null"`
 	CreatedAt   time.Time `gorm:"default:CURRENT_TIMESTAMP"`
--- domain/entity/comment.go
+++ domain/entity/comment.go
@@ -5,9 +5,9 @@ import "time"
 type Comment struct {
 	ID            uint
 	Comment       string `gorm:"size:200;not null"`
-	AppointmentID uint
+	AppointmentID uint   `gorm:"not null"`
 	Appointment   Appointment
-	UserID        uint
+	UserID        uint `gorm:"not null"`
 	User          User
 	CreatedAt     time.Time `gorm:"default:CURRENT_TIMESTAMP"`
 	UpdatedAt     time.Time `gorm:"default:CURRENT_TIMESTAMP"`
