97fa19cc9666a847935eb544b01d6e5e602c2477
wchrth <watcharathorn@icloud.com>
2025-03-04 16:24:06 +0700

    change method

--- internal/route/auth_route.go
+++ internal/route/auth_route.go
@@ -10,8 +10,8 @@ func SetupAuthRoute(router *gin.Engine, authHandler *handler.AuthHandler) {
 
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/auth/login", authHandler.Login)
-		v1.GET("/auth/refresh", authHandler.RefreshToken)
+		v1.POST("/auth/login", authHandler.Login)
+		v1.POST("/auth/refresh", authHandler.RefreshToken)
 	}
 
 }
