2e972dffc8fe8c3a775263212d5e815e53e2c8b4
wchrth <watcharathorn@icloud.com>
2025-03-05 16:39:10 +0700

    add appointment history model

--- internal/model/appointment.go
+++ internal/model/appointment.go
@@ -17,3 +17,11 @@ type Appointment struct {
 	UpdatedDate time.Time                  `json:"updated_date" db:"updated_date"`
 	UpdatedBy   string                     `json:"updated_by" db:"updated_by"`
 }
+
+type AppointmentHistory struct {
+	ID            int64                      `json:"id" db:"id"`
+	Title         string                     `json:"title" db:"title"`
+	Description   string                     `json:"description" db:"description"`
+	Status        constant.AppointmentStatus `json:"status" db:"status"`
+	AppointmentID int64                      `json:"appointment_id" db:"appointment_id"`
+}
